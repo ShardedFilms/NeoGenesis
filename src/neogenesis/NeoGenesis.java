a3f2767b3666f14221945f158c5653899f9dee7d
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2023-05-13 14:01:17 +0700

    Information

--- src/neogenesis/NeoGenesis.java
+++ src/neogenesis/NeoGenesis.java
@@ -25,7 +25,38 @@ import static mindustry.Vars.*;
 
 public class NeoGenesis extends Mod{
 	
-	public NeoGenesis(){
+	public NeoGenesis(
+
+        // Base Category.
+        // 0 = Devlopment
+        //
+        // 1 = Genesux
+        // 2 = Solran
+        // 3 = Quantra
+        // 4 = Elecian
+        // 5 = Purtuxe
+        // 6 = Hexalia
+        // 7 = Nexolare
+        // 8 = Transentium
+        // 9 = Etherna
+        // a = Serpulo(Special)
+        // b = Gemini
+        // c = lyra 
+        // d = taurus
+        // e = Electra
+        // f = Erekir(Special)
+        // g = Assertion
+        // h = Pink
+        // i = Silver
+        // j = orange
+        // k = Lightblue
+        // l = accent
+        //
+        // Base > Category > index > Content
+        //
+        // All content is ordered alphabeticial
+        // You must sort it yourself.
+    ){
         Log.info("Loads Constructor");
     }
 
