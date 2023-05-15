3d54d695d8c9ee957450ff846123fcd8ca3ed68b
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2023-05-15 22:24:59 +0700

    generator

--- src/neogenesis/content/NGPlanets.java
+++ src/neogenesis/content/NGPlanets.java
@@ -20,7 +20,7 @@ public class NGPlanets {
 
     public static Planet
     // one
-    siravax;
+    siravax,siratla;
 
     public static void load(
 
@@ -47,6 +47,38 @@ public class NGPlanets {
                 NGColor.genesux3
             );
         }};
+        siratla = new Planet("z-pt-z", siravax , 1f, 3){{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+            );
+
+            launchCapacityMultiplier = 0.5f;
+            sectorSeed = 2;
+            allowWaves = true;
+            allowWaveSimulation = true;
+            allowSectorInvasion = true;
+            allowLaunchSchematics = true;
+            enemyCoreSpawnReplace = true;
+            allowLaunchLoadout = true;
+            //doesn't play well with configs
+            prebuildBase = false;
+            ruleSetter = r -> {
+                r.waveTeam = Team.blue;
+                r.placeRangeCheck = false;
+                r.showSpawns = true;
+            };
+            iconColor = NGColor.genesux2;
+            atmosphereColor = NGColor.genesux3;
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+        }};
+
  
 
     }
